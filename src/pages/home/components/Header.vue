500e2ecb9521e41266988a7e8531d541f9b68145
2016soft <2016soft@iMac19.local>
2018-10-12 11:53:19 +0800

    add home header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="header">
+        <div class="header_left">
+            zuo
+        </div>
+        <div class="headel_input">
+            zhong   
+        </div>
+        <div class="header_right">
+            you
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'HomeHeader',
+    
+}
+</script>
+
+<style scoped>
+.header{
+    height: 100px;
+    background-color:red;
+}
+.header_left{
+    float: left;
+}
+.header_input{
+    float: left;
+    background-color: white;
+    border-radius: 3px;
+}
+.header_right{
+    float: left;
+}
+
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        swiper
+    </div>
+</template>
+
+<script>
+export default{
+    name:'HomeSwiper',
+    
+}    
+</script>
+
+<style scoped>
+
+</style>
